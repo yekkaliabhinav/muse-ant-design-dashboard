b5439b07f6449149fee3843fb619a1bcbee58eae
Abhinav Sai Yekkali <115100076+yekkaliabhinav@users.noreply.github.com>
2024-10-25 23:38:13 +0530

    Update Tables.js

--- src/pages/Tables.js
+++ src/pages/Tables.js
@@ -592,31 +592,7 @@ function Tables() {
   return (
     <>
       <div className="tabled">
-        <Row gutter={[24, 0]}>
-          <Col xs="24" xl={24}>
-            <Card
-              bordered={false}
-              className="criclebox tablespace mb-24"
-              title="Authors Table"
-              extra={
-                <>
-                  <Radio.Group onChange={onChange} defaultValue="a">
-                    <Radio.Button value="a">All</Radio.Button>
-                    <Radio.Button value="b">ONLINE</Radio.Button>
-                  </Radio.Group>
-                </>
-              }
-            >
-              <div className="table-responsive">
-                <Table
-                  columns={columns}
-                  dataSource={data}
-                  pagination={false}
-                  className="ant-border-space"
-                />
-              </div>
-            </Card>
-
+        
             <Card
               bordered={false}
               className="criclebox tablespace mb-24"
